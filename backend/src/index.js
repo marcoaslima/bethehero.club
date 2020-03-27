a97777bc18a4fb2fcf927e80e36ffd43df2ea958
Marco Aurelio Lima <marckdx@outlook.com>
2020-03-27 00:25:29 -0300

    feat: backend finished

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+
+const routes  = require('./routes/api')
+
+const app = express()
+
+// app.use(cors({
+//     origin: 'https://bethehero.club'
+// }))
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
